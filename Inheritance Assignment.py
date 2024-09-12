3bbc01db69fc2d42e0d7fccae83ba7b3c76e3b7d
ozansa <159722371+ozansa@users.noreply.github.com>
2024-09-12 09:58:41 +0100

    Classes and inheritance assignment

---
+++ Inheritance Assignment.py
@@ -0,0 +1,11 @@
+#parent class
+class User:
+name = 'John'
+email = 'john@gmail.com' password = '123abc'
+#first class inheriting from User
+class Manager (User):
+base_pay = 15.00
+department = 'General'
+#second class inheriting from User
+class Employee (User):
+mailing address = '' mailing_list = True
